d082ed1c5aae0f08b9e32005b72c2ddca17a427a
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2017-11-21 19:41:08 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,27 @@
+# ragav
+#include<stdio.h>
+void main ()
+{
+char letter;
+printf(" enter the let");
+scanf("%c",&letter);
+switch(letter)
+case a:
+printf("v");
+break;
+case e:
+printf("v");
+break;
+case i:
+printf("v");
+break;
+case o:
+printf("v");
+break;
+case u:
+printf("v);
+break;
+default:
+printf("no");
+break;
+}
