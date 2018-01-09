e4fd9c4738752aa6fde06b335a2f7b04667e853e
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-01-09 10:43:21 +0000

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,14 @@
 # include<stdio.h>
 void main()
 {
-char a[20],n;
-printf("enter the number:%d",n);
-scanf("%s",&a[20]);
-for(int i=0;i<=n;i++)
-{
-printf("hello world");
+char a[20];
+  int n;
+  printf("enter the number:%d",n);
+  scanf("%s",a);
+  for(int i=0;i<=n;i++)
+  {
+    printf("hello");
+  }
 }
-}
+
+  
