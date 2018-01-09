a8f2f6516b1c8231fe5b1e360b1ebbcc5cf2195e
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-01-09 10:23:00 +0000

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+{
+char letter;
+printf("enter the value");
+scanf("%c",&letter);
+switch (letter)
+case 1:
+printf("alphabet");
+break;
+case default:
+printf("not");
+break;
+}
