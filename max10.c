361c82a45f1aba8995fb70dc1405bc1b9b61cd12
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-01-18 16:35:08 +0530

    Create max10.c

---
+++ max10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a[10],n;
+for(int i=0;i<=n;i++)
+{
+scanf("%d",&a[1]);
+}
+for(int i=0;i<=n;i++)
+{
+for(int j=0;j<=n;j++)
+{
+if(a[i]>a[j])
+{
+temp =a[i];
+}
+printf("a[i]");
+}
+}
