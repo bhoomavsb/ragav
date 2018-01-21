4cda4dce597391ce7bd97677b3ccc845c212c9de
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-01-21 16:57:00 +0530

    Create arithdiv.c

---
+++ arithdiv.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+int a,b,c,d;
+float e;
+scanf("%d %d",&a ,&b);
+d=a*b;
+printf("%d\n",d);
+scanf("%d",&c);
+e=d/c;
+printf("%f",e);
+
+// your code goes here
+	return 0;
+}
