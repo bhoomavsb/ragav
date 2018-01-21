28d7b3d8256228b47fed3bbcfb0a5a426f3c0e42
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-01-21 16:14:17 +0530

    Create sumofnumb.c

---
+++ sumofnumb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i,n,sum=0;
+	scanf("%d",&n);
+	printf("enter the number:%d\n",n);
+	for(i=0;i<=n;i++)
+	{
+		sum=sum+i;
+	}
+	printf("the sum is:%d",sum);
+	
+
+	return 0;
+}
+
