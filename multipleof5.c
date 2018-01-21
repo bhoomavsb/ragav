2054515d3cdd48c087b5250d270cddca96235365
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-01-21 14:15:42 +0530

    Create multipleof5.c

---
+++ multipleof5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int i,product,n;
+scanf("%d",&n);
+printf("enter the number:%d\n",n);
+
+for(i=1;i<=n;i++)
+{
+	product=5*i;
+	
+	
+
+printf("%d\t",product);
+}
+
+	return 0;
+}
