6dc7bc81f32b7df5ed663ee3d4017507af425671
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-01-23 16:57:47 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int d,n,t,s;
+	scanf("%d",&n);
+	while(n>=1)
+	{
+		s=s*10+d;
+		n=t/10;
+		printf("%d",s);
+		
+	}
+	// your code goes here
+	return 0;
+}
