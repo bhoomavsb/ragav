d20ab48ee7e2b68e6b6a6862fd050e929e6c681e
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-01-23 16:13:54 +0530

    Update and rename strcmp.c to strcat.c

---
+++ strcat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+char a[20];
+char b[20];
+int i,j;
+ 
+	for(i=0;i<=j;i++)
+	{
+		if(a[20]>b[20])
+		{
+			printf("%s\n");
+ 
+		}
+ 
+	}// your code goes here
+	return 0;
+}
