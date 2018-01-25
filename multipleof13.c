67b0359b4e43b86e4b386aecb256a89c4ae3bd9f
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-01-25 16:55:32 +0530

    Create multipleof13.c

---
+++ multipleof13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	scanf("%d",&n);
+	if(n%13==0)
+	{
+		printf("yes");
+		
+	}
+	else
+	{
+		printf("no");
+	}
+	// your code goes here
+	return 0;
+}
