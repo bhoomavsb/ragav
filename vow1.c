85b29ec6f0a26337839b8e4a79dc1d8a117d3896
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-02-01 15:06:53 +0530

    Create vow1.c

---
+++ vow1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+	char a[10];
+	scanf("%s",&a[10]);
+	if(a[10]=='a'||a[10]=='e'||a[10]=='i'||a[10]=='o'||a[10]=='u')
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	
+	return 0;
+}
