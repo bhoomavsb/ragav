6d43eb302fc94c29102c20942e26534495432b6a
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-02-08 14:59:48 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+char a[50],b[50];
+scanf("%s %s",&a,&b);
+if(a[50]&&b[50])
+{
+	printf("yes");
+}
+else
+{
+	printf("no");
+}
+return 0;
+}
+
