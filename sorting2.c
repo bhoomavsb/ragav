d0cad7956e535ac07fe14fa51dc071ef9d522105
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-02-09 14:53:47 +0530

    Create sorting2.c

---
+++ sorting2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+int a[10],i,j,n,t;
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=1;i<n;i++)
+{
+	for(j=i+1;j<n;j++)
+	{
+		if(a[i]>a[j])
+		{
+			t=a[i];
+			a[i]=a[j];
+			a[j]=t;
+		}
+	}
+printf("%d\t",a[i]);
+	
+}
+	return 0;
+}
