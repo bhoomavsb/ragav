9257319a0204c4c9bfba323ce5aedb6343ecd868
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-02-20 16:18:22 +0530

    Create kthsmallnum.c

---
+++ kthsmallnum.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n,k,i,t,j,m,a[50];
+	scanf("%d %d",&n,&k);
+	scanf("%d",&m);
+	for(i=1;i<=m;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=1;i<=m;i++)
+	{
+		for(j=1;j<=m;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	for(i=1;i<=m;i++)
+	{
+		if(a[i]==k)
+		{
+			
+		}
+	}
+	
+
+printf("Kth odd number is %d",k);	
+	return 0;
+}
